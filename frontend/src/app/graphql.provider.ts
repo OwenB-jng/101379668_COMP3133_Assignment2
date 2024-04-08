a225249649ee80135e47be7d1691231392409d8e
OwenB-jng <owenbeattie1@gmail.com>
2024-04-07 21:14:30 -0400

    changed connection to backend

--- frontend/src/app/graphql.provider.ts
+++ frontend/src/app/graphql.provider.ts
@@ -3,7 +3,7 @@ import { HttpLink } from 'apollo-angular/http';
 import { ApplicationConfig, inject } from '@angular/core';
 import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
 
-const uri = 'http://localhost:4000/graphql';
+const uri = 'https://one01379668-comp3133-assignment2.onrender.com';
 export function apolloOptionsFactory(): ApolloClientOptions<any> {
   const httpLink = inject(HttpLink);
   return {
